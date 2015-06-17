6d0e8454011994e5bfb7b73832d27b5a02f25011
Chinmay Pai <chinmay3.14@hotmail.com>
2015-06-17 16:08:48 +0530

    Fixed Download Crash v.1.3.4.

--- app/src/main/java/io/github/UltimateBrowserProject/Browser/UltimateBrowserProjectDownloadListener.java
+++ app/src/main/java/io/github/UltimateBrowserProject/Browser/UltimateBrowserProjectDownloadListener.java
@@ -17,21 +17,7 @@ public class UltimateBrowserProjectDownloadListener implements DownloadListener 
     }
 
     @Override
-    public void onDownloadStart(final String url, String userAgent, final String contentDisposition, final String mimeType, long contentLength) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(context);
-        builder.setCancelable(false);
-
-        builder.setTitle(R.string.dialog_title_download);
-        builder.setMessage(URLUtil.guessFileName(url, contentDisposition, mimeType));
-
-        builder.setPositiveButton(R.string.dialog_button_positive, new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                BrowserUnit.download(context, url, contentDisposition, mimeType);
-            }
-        });
-
-        builder.setNegativeButton(R.string.dialog_button_negative, null);
-        builder.create().show();
+    public void onDownloadStart(String url, String userAgent, String contentDisposition, String mimeType, long contentLength) {
+        BrowserUnit.download(context, url, contentDisposition, mimeType);
     }
 }
