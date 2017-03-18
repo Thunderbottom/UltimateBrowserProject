f11f83ecc2dcc02d80e8ac619d0402383cd99790
Simao Gomes Viana <xdevs23@outlook.com>
2017-03-18 16:22:55 +0100

    Get everything up-to-date
    Also get rid of crashlytics for now

--- app/src/main/java/io/github/UltimateBrowserProject/Application/UltimateBrowserProjectApplication.java
+++ app/src/main/java/io/github/UltimateBrowserProject/Application/UltimateBrowserProjectApplication.java
@@ -1,19 +1,12 @@
 package io.github.UltimateBrowserProject.Application;
 
-
 import android.app.Application;
-
-import com.crashlytics.android.Crashlytics;
-
-import io.fabric.sdk.android.Fabric;
-
 
 public class UltimateBrowserProjectApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
-        Fabric.with(this, new Crashlytics());
     }
 
 }
